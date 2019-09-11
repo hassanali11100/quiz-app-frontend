577b23e09f0cfcc2b3fdb1ac80d6a8f07c10bda2
hassanali11100 <hassanali11100@gmail.com>
2019-09-11 12:26:28 +0500

    feat/crud delete quiz functionality started

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { QuizData } from './quiz-data.service';
 import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
@@ -18,7 +18,8 @@ import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
     AppRoutingModule,
     InMemoryWebApiModule.forRoot(QuizData),
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
