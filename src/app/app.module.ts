0f6649592584625033e2b0a72b45a7976bb633df
hassanali11100 <hassanali11100@gmail.com>
2019-09-11 09:33:49 +0500

    feat/quiz-crud in-memory-web-api package added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { QuizData } from './quiz-data.service';
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    InMemoryWebApiModule.forRoot(QuizData)
   ],
   providers: [],
   bootstrap: [AppComponent]
