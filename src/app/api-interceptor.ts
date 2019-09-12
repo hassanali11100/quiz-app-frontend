ed03ee861325f2be897c5855071db1af07f23906
hassanali11100 <hassanali11100@gmail.com>
2019-09-12 11:53:07 +0500

    baseUrl hardcoded

--- src/app/api-interceptor.ts
+++ src/app/api-interceptor.ts
@@ -7,6 +7,8 @@ import { environment } from '../environments/environment';
 @Injectable()
 export class ApiInterceptor implements HttpInterceptor {
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        // TODO: fix this via env-var, heroku doesn't pick env variable by mention --prod flag
+        const temp_env_var_baseUrl: string = 'https://anyquizapi.herokuapp.com/';
         const apiReq = req.clone({ url: `${environment.baseUrl}${req.url}` });
         return next.handle(apiReq);
     }
