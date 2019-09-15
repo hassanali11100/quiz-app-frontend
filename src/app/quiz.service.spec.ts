8e4044347d96070cdb128e41a72f7631540816d0
hassanali11100 <hassanali11100@gmail.com>
2019-09-15 20:10:16 +0500

    getQuizzes() test added

---
+++ src/app/quiz.service.spec.ts
@@ -0,0 +1,47 @@
+import { TestBed } from '@angular/core/testing';
+
+import { QuizService } from './quiz.service';
+import { HttpClient } from '@angular/common/http';
+import { of } from 'rxjs';
+
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing'
+
+fdescribe('QuizService', () => {
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
+  let quizService: QuizService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [
+        QuizService
+      ]
+    })
+
+    httpClient = TestBed.get(HttpClient);
+    httpTestingController = TestBed.get(HttpTestingController);
+    quizService = TestBed.get(QuizService);
+  });
+
+  it('should be created', () => {
+    expect(quizService).toBeTruthy();
+  });
+
+  it('#getQuizzes should return expected Quizzes', () => {
+    const expectedQuizzes = [
+      {id: 1, title: 'kashmir issue', description: 'it contains questions regarding kashmir'},
+      {id: 2, title: 'economic crisis', description: 'it contains question regarding economy'}
+    ];
+
+    quizService.getQuizzes().subscribe(
+      (quizzes) => expect(quizzes).toEqual(expectedQuizzes),
+      fail
+    );
+
+    const req = httpTestingController.expectOne('/quizzes');
+    expect(req.request.method).toEqual('GET');
+
+    req.flush(expectedQuizzes);
+  });
+});
