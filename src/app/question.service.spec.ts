c5db039562535692cbbe46933c05ecad542c7e61
hassanali11100 <hassanali11100@gmail.com>
2019-09-20 12:07:01 +0500

    routing fixed

---
+++ src/app/question.service.spec.ts
@@ -0,0 +1,59 @@
+import { TestBed } from '@angular/core/testing';
+
+import { QuestionService } from './question.service';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { HttpClient } from '@angular/common/http';
+
+describe('QuestionService', () => {
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
+  let questionService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule,
+      ],
+      providers: [
+        QuestionService
+      ]
+    });
+
+    httpClient = TestBed.get(HttpClient);
+    httpTestingController = TestBed.get(HttpTestingController);
+    questionService = TestBed.get(QuestionService);
+  });
+
+  it('should be created', () => {
+    const service: QuestionService = TestBed.get(QuestionService);
+    expect(service).toBeTruthy();
+  });
+
+  it('#getQuestions should fetch question of a quiz', () => {
+    const stubbedQuestions = [
+      {
+          id: 3,
+          text: "This is a test question",
+          quiz_id: 1,
+          created_at: "2019-09-05T06:10:42.471Z",
+          updated_at: "2019-09-05T06:10:42.471Z"
+      },
+      {
+          id: 4,
+          text: "This is a 2nd test question",
+          quiz_id: 1,
+          created_at: "2019-09-05T07:20:31.971Z",
+          updated_at: "2019-09-05T07:20:31.971Z"
+      }
+    ];
+
+    questionService.getQuestions(1).subscribe(
+      data => expect(data).toEqual(stubbedQuestions)
+    );
+
+    const req = httpTestingController.expectOne('quizzes/1/questions');
+    expect(req.request.method).toEqual('GET');
+
+    req.flush(stubbedQuestions);
+  });
+});
